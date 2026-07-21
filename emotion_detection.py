64961cfe07df192a2addf1dcb7c58f159a49d994
celinekuttler <194842828+celinekuttler@users.noreply.github.com>
2026-07-21 06:12:14 -0400

    add formatted output

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,5 +6,8 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobject =  { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobject, headers=headers)
-    return response.text
-    # not json.loads(response.text)
+    t = json.loads(response.text)
+    e = t["emotionPredictions"][0]["emotion"]
+    max_emotion = max(e, key=e.get)
+    e["dominant_emotion"] = max_emotion
+    return e
